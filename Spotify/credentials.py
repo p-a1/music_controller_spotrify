951bf9e55a0206582ea973c5d8474f8085006408
pc_1 <127506349+p-a1@users.noreply.github.com>
2024-11-24 14:52:47 +0200

    Update credentials.py
    hidden the secret key

--- Spotify/credentials.py
+++ Spotify/credentials.py
@@ -1,3 +1,3 @@
-CLIENT_ID='6f9162dd15124d948b5a40f0166e99c6'
-CLIENT_SECRET='1d38d33c95da47e787b4863f8d9902ce'
+CLIENT_ID='****************************'
+CLIENT_SECRET='****************************'
 REDIRECT_URL='http://192.168.1.3:8000/spotify/redirect'
